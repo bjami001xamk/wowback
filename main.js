c3e86de2516edad4db06a43f923a16deaa0235b3
Jani Miettinen <bjami001@edu.xamk.fi>
2021-02-28 12:26:41 +0200

    Testing

--- main.js
+++ main.js
@@ -22,12 +22,7 @@ if (process.env.REDISTOGO_URL) {
 }
 
 
-app.use(cors({
-    credentials: true,
-    origin: [
-        'https://pedantic-nightingale-fe0a38.netlify.app/'
-    ]
-}));
+app.use(cors());
 //app.use(require('serve-static')(__dirname + '/../../public'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
