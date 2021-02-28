6d64df32bf68924c0317096f6a9968d9b6bf5144
Jani Miettinen <bjami001@edu.xamk.fi>
2021-02-28 22:51:41 +0200

    Testing

--- main.js
+++ main.js
@@ -37,6 +37,7 @@ app.use(session({
                 cookie: {
                     secure: false,
                     httponly:false,
+                    maxAge: 60000000
                 }
             }));
 
