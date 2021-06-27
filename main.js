52820526fd04e6805ccfd84587efe1cb3d41231d
Jani Miettinen <bjami001@edu.xamk.fi>
2021-06-27 19:32:08 +0300

    testing passport

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ let RedisStore = require('connect-redis')(session);
 let rtg   = require("url").parse(process.env.REDISTOGO_URL);
 let redisClient = require("redis").createClient(rtg.port, rtg.hostname);
 redisClient.auth(rtg.auth.split(":")[1]);
-
+const callBackUrl = "https://wowback.herokuapp.com/auth/bnet/callback";
 
 app.set('trust proxy', 1);
 app.use(cors({
@@ -38,7 +38,7 @@ app.use(session({
 passport.use(new BnetStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://localhost:3000/oauth/battlenet/callback",
+    callbackURL: callBackUrl,
     region: "eu"
 }, function(accessToken, refreshToken, profile, done) {
     return done(null, profile);
