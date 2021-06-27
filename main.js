2f82823b8defb7fbef5fda9e6125c7d34a51b86d
Jani Miettinen <bjami001@edu.xamk.fi>
2021-06-27 23:08:56 +0300

    refactoring passport

--- main.js
+++ main.js
@@ -4,31 +4,18 @@ let cors = require('cors');
 const controller = require('./controller');
 const port = process.env.PORT || 8000;
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const BnetStrategy = require('passport-bnet').Strategy;
 
 let session = require('express-session');
 let RedisStore = require('connect-redis')(session);
 let rtg   = require("url").parse(process.env.REDISTOGO_URL);
 let redisClient = require("redis").createClient(rtg.port, rtg.hostname);
 redisClient.auth(rtg.auth.split(":")[1]);
-const callBackUrl = "https://wowback.herokuapp.com/auth/bnet/callback";
 
 app.set('trust proxy', 1);
 app.use(cors({
     credentials: true,
     origin:  ['https://pedantic-nightingale-fe0a38.netlify.app',"http://localhost:3000","https://localhost:3000"],
 }));
-
-passport.use(new BnetStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: callBackUrl,
-    region: "eu"
-}, function(accessToken, refreshToken, profile, done) {
-    return done(null, profile);
-}));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(session({
@@ -44,16 +31,8 @@ app.use(session({
                     sameSite: 'none'
                 }
             }));
-app.use(passport.initialize());
-app.use(passport.session());
 
-passport.serializeUser(function(user, done){
-    done(null, user);
-  });
-  
-  passport.deserializeUser(function(user, done){
-    done(null, user);
-  });
+const passport = require('./passport');
             
 app.use('/', controller);
 
---
+++ passport.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const BnetStrategy = require('passport-bnet').Strategy;
+const callBackUrl = "https://wowback.herokuapp.com/auth/bnet/callback";
+
+passport.use(new BnetStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: callBackUrl,
+    region: "eu"
+}, function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done){
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done){
+    done(null, user);
+  });
+
+module.exports = passport
